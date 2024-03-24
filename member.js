56443a1ca74b4ab98db0a4bc50039d40268b9ef3
kselsey <90426947+kselsey@users.noreply.github.com>
2024-03-24 18:42:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+    var skills = {
+        html: 5,
+        css: 5,
+        javascript: 5,
+        jquery: 5,
+        angular: 5,
+        react: 5,
+        vue: 5,
+        node: 5,
+        mongodb: 5,
+        mysql: 5,
+        php: 5,
+        python: 5
+    };
+    return skills;
+}
